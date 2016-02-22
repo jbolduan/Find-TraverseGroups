dde9ddfa5e3a08f0eb96e9aac9545b21fa784d56
Jeff Bolduan <jbolduan@umn.edu>
2016-02-22 13:01:28 -0600

    Code should now work locally and on a file share.

--- Find-TraverseGroups.ps1
+++ Find-TraverseGroups.ps1
@@ -1,12 +1,15 @@
-﻿[CmdletBinding()]
+﻿<#
+	TODO: Add comment based help
+#>
+[CmdletBinding()]
 param(
-	[Parameter(Mandatory=$true)][string]$Path="C:\Users\jbolduan\Desktop\testtrav"
+	[Parameter(Mandatory=$true)][string]$Path
 )
 
 # Import active directory module
 Import-Module ActiveDirectory
 
-$TraveerseGroups = @()
+$TraverseGroups = @()
 
 # Build tests for traverse group
 $travRights = [System.Security.AccessControl.FileSystemRights]"ReadAndExecute,Synchronize"
@@ -14,36 +17,47 @@ $travInheritanceFlag = [System.Security.AccessControl.InheritanceFlags]::None
 $travPropagationFlag = [System.Security.AccessControl.PropagationFlags]::None
 $travType = [System.Security.AccessControl.AccessControlType]::Allow
 
+# Get the root of the path
+if($Path.StartsWith("\\")) {
+	$RootPath = "\\" + [string]::Join("\",$Path.Split("\")[2])
+} else {
+	$RootPath = Split-Path -Path $Path -Qualifier
+}
+
 # Split up paths and pull the root
-$spath = $Path.Split("\")
-$qpath = Split-Path -Path $Path -Qualifier
+$spath = $Path.Replace("$RootPath\", "").Split("\")
 $Paths = New-Object System.Collections.ArrayList
 
 # Build a list of all the directories that lead to the target directory
 for($i = 0; $i -le $spath.Length; $i++) {
-    if($spath[$i] -ne $null) {
-        $PathToAdd = ""
-        for($j = 0; $j -le $i; $j++) {
-            $PathToAdd += "$($spath[$j])\"
-        }
-        $Paths.Add($PathToAdd) | Out-Null
-    }
+	if($spath[$i] -ne $null) {
+		$PathToAdd = ""
+		for($j = 0; $j -le $i; $j++) {
+			$PathToAdd += "$($spath[$j])\"
+		}
+		# Add the new path to our list of paths
+		$Paths.Add("$RootPath\$PathToAdd") | Out-Null
+	}
 }
 
+# Loop through the paths and determine which contain a traverse group
 foreach($item in $Paths) {
-    $itemacl = Get-Acl -Path $item
-    Write-Verbose $item
-    foreach($acl in $itemacl.Access) {
-        if(($acl.InheritanceFlags -eq $travInheritanceFlag) -and ($acl.PropagationFlags -eq $travPropagationFlag) -and ($acl.FileSystemRights -eq $travRights) -and ($acl.AccessControlType -eq $travType)) {
-            Write-Verbose "Potential traverse group: $($acl.IdentityReference)"
+	$itemacl = Get-Acl -Path $item
+		foreach($acl in $itemacl.Access) {
+		# Check the acl for the traverse permissions defined earlier
+		if(($acl.InheritanceFlags -eq $travInheritanceFlag) -and ($acl.PropagationFlags -eq $travPropagationFlag) -and ($acl.FileSystemRights -eq $travRights) -and ($acl.AccessControlType -eq $travType) -and ($acl.IsInherited -eq $false)) {
+			# We've now found a traverse group
 			$SamAccountName = $acl.IdentityReference.ToString().Split("\")[1]
+
+			# Make sure the account name isn't null, then get the group make sure it exists in AD then add the path to the group output object.
 			if($null -ne $SamAccountName) {
-				Write-Verbose "Testing AD Permissions: $SamAccountName"
 				$ADObject = Get-ADGroup -Identity $SamAccountName
 				if($ADObject -ne $null) {
-					
+					$TraverseGroups += $ADObject | Add-Member -MemberType NoteProperty -Name TraversePath -Value $item -Force -PassThru
 				}
 			}
-        }
-    }
+		}
+	}
 }
+
+return $TraverseGroups
